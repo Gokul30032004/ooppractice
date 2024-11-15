3df7358d6f06157b5460997e6b0556cdc164584d
Gokul300304 <07gokul2004@gmail.com>
2024-11-15 13:02:34 +0530

    git push practice file

---
+++ prtest.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class prgit test{
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int a = 10;
+    int b = 10;
+    System.out.println(a +b);
+}}
